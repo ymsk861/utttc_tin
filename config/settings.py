850178c8cbeaedc0516529b9dc5dc4ba51951ab8
ymsk <yamasaki-shun219@g.ecc.u-tokyo.ac.jp>
2020-05-07 16:29:46 +0900

    #3最後まで・管理画面にアクセス可

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q4df0-(@vm@$8wzhll&y!bp$%0ie=wo-_cw+n9i83pfy^fvtdo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 
 # Application definition
