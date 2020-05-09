ed770ba78b1ce0c7f2da84066577ea1a68d43584
Aki-373 <kusamoch@gmail.com>
2020-05-09 14:31:12 +0900

    #5

--- cms/urls.py
+++ cms/urls.py
@@ -6,4 +6,7 @@ app_name = 'cms'
 
 urlpatterns = [
     path('', views.TopView.as_view(), name='top'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('signup/', views.UserCreate.as_view(), name='signup'),
 ]
