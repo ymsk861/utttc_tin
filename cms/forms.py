c65f877f6a6de0cdccf00f82ce61800ee14b7aea
李　璐 <lilu@rinoMacBook.local>
2020-05-09 15:27:20 +0900

    #5

--- cms/forms.py
+++ cms/forms.py
@@ -1,8 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django import forms
 
 UserModel = get_user_model()
 
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields = ('username', 'first_name', 'last_name', 'email','twitter')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'input'
 
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
--- cms/urls.py
+++ cms/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('', views.TopView.as_view(), name='top'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
-    path('signup/', views.UserCreate.as_view(), name='signup'),
+    path('signup/', views.UserCreate.as_view(),name='signup'),
+    path('user/<int:pk>/update/', views.UserUpdate.as_view(), name='user_update'),
+    path('user/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+    path('user/', views.UserList.as_view(), name='user_list'),
+    path('user/<int:pk>/delete/', views.UserDelete.as_view(), name='user_delete'),
 ]
