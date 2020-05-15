018e7f32d4bc511b52c490c8549e56bc16c3384b
ymsk <yamasaki-shun219@g.ecc.u-tokyo.ac.jp>
2020-05-15 19:40:20 +0900

    add_htmlfiles

--- cms/urls.py
+++ cms/urls.py
@@ -6,7 +6,7 @@ app_name = 'cms'
 
 urlpatterns = [
     path('', views.TopView.as_view(), name='top'),
-    path('next/', views.NextView.as_view(),name='next'),
+    path('circleid-1-2/', views.NextView.as_view(), name='circleid-1-2'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
     path('signup/', views.UserCreate.as_view(),name='signup'),
